6ac08e687fbec1c4ff993d0ee9be42e0814988ae
Bartha Ariana Janka <ariabartha@gmail.com>
2023-10-27 12:41:24 +0200

    konzolba kiírva a feladat szövege

--- feladat03.js
+++ feladat03.js
@@ -1,4 +1,5 @@
 var lista = [0, 7, 16, 30, 41];
-for (i = lista.length; i >= 0; i--) {
+console.log("A lista elmei a végéről kezdve:");
+for (let i = lista.length - 1; i >= 0; i--) {
     console.log(lista[i]);
 }
